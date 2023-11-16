b501a9a1d3d7968bad0862a5d930422de718eeb9
minhddph25260 <139239663+Minhdd15112003@users.noreply.github.com>
2023-11-17 02:52:58 +0700

    add handlebar

---
+++ blog/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars"); // Thay handlebars bằng express-handlebars
+const path = require("path");
+const { log } = require("console");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+// Sử dụng create method của express-handlebars để tạo instance
+app.engine("handlebars", exphbs.create({ extname: ".hbs" }).engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "resources/views"));
+console.log("====================================");
+console.log("PATH: ", path.join(__dirname, "resources/views"));
+console.log("====================================");
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
