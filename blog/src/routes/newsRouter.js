598c162bb3b4fe81fa21e4059c170163d3da3b2d
minhdd <minhdd15112003@gmail.com>
2023-11-30 12:42:27 +0700

    df

--- blog/src/routes/newsRouter.js
+++ blog/src/routes/newsRouter.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const newsController = require("../app/controller/NewsController");
+const newsController = require('../app/controller/NewsController');
 
 // newsController.index là chuyền fun từ bên news controller
-router.use("/:slug", newsController.show);
-router.use("/", newsController.news);
+router.use('/:slug', newsController.show);
+router.use('/', newsController.news);
 module.exports = router;
